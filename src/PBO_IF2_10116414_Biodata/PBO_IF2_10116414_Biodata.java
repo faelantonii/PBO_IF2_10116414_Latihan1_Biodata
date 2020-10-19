15cb2aa780701c30ff7751797628f0a19f449abd
sweenutt <sweenutt@192.168.3.19>
2020-10-20 01:17:58 +0700

    Upload Latihan 1 Biodata

---
+++ src/PBO_IF2_10116414_Biodata/PBO_IF2_10116414_Biodata.java
@@ -0,0 +1,36 @@
+package PBO_IF2_10116414_Biodata;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author sweenutt
+ * NAMA         : FAEL ANTONI WIJAYA
+ * KELAS        : PBO-2
+ * NIM          : 10116414
+ * Deskripsi    : Program Biodata
+ */
+public class PBO_IF2_10116414_Biodata {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("-------------------------------------------------");
+        System.out.println("BIODATA SAYA" + "\n" + "-------------------------");
+        System.out.println("NAMA\t\t:   Fael Antoni WIjaya");
+        System.out.println("ALAMAT\t\t:   Jln. Titimplik Dalam No.1S/151C");
+        System.out.println("TEMPAT LAHIR\t:   Cirebon");
+        System.out.println("TANGGAL LAHIR\t:   10 Agustus 1998");
+        System.out.println("-------------------------------------------------");
+        System.out.println("HOBI\t\t:   Browsing Internet");
+        System.out.println("MAKANAN FAVORIT\t:   Semua Suka");
+        System.out.println("MINUMAN FAVORIT\t:   Jus Jambu");
+        System.out.println("-------------------------------------------------");
+        
+        
+    }
+    
+}
